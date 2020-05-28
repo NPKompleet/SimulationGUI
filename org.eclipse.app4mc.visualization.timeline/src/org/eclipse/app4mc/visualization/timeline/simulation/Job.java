f46358aeaf3220a86b66df2c07bce15eebee2798
Philip <filip2unine@yahoo.co.uk>
2020-05-28 14:45:17 +0200

    feat: Added DESMO-J; a Java DES library. Closes #8

---
+++ org.eclipse.app4mc.visualization.timeline/src/org/eclipse/app4mc/visualization/timeline/simulation/Job.java
@@ -0,0 +1,20 @@
+package org.eclipse.app4mc.visualization.timeline.simulation;
+
+import co.paralleluniverse.fibers.SuspendExecution;
+import desmoj.core.simulator.Model;
+import desmoj.core.simulator.SimProcess;
+
+public class Job extends SimProcess {
+
+	public Job(Model arg0, String arg1, boolean arg2) {
+		super(arg0, arg1, arg2);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void lifeCycle() throws SuspendExecution {
+		// TODO Auto-generated method stub
+
+	}
+
+}
