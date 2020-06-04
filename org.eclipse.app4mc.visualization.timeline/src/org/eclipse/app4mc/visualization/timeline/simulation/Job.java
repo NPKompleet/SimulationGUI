9a4c8cba2f3ea0798683ef1225361f23c516e460
Philip <filip2unine@yahoo.co.uk>
2020-06-04 12:26:40 +0200

    Added initial code implementation for Task process mainly, little Job process

--- org.eclipse.app4mc.visualization.timeline/src/org/eclipse/app4mc/visualization/timeline/simulation/Job.java
+++ org.eclipse.app4mc.visualization.timeline/src/org/eclipse/app4mc/visualization/timeline/simulation/Job.java
@@ -5,6 +5,20 @@ import desmoj.core.simulator.Model;
 import desmoj.core.simulator.SimProcess;
 
 public class Job extends SimProcess {
+	private Task task;
+	private int executionTime;
+	private int deadline;
+	private SimModel model;
+	String name;
+
+	public Job(Task task, String name, int executionTime, int deadline, Model model) {
+		super(model, name, false);
+		this.task = task;
+		this.name = name;
+		this.executionTime = executionTime;
+		this.deadline = deadline;
+		this.model = (SimModel) model;
+	}
 
 	public Job(Model arg0, String arg1, boolean arg2) {
 		super(arg0, arg1, arg2);
--- org.eclipse.app4mc.visualization.timeline/src/org/eclipse/app4mc/visualization/timeline/simulation/Task.java
+++ org.eclipse.app4mc.visualization.timeline/src/org/eclipse/app4mc/visualization/timeline/simulation/Task.java
@@ -1,20 +1,41 @@
 package org.eclipse.app4mc.visualization.timeline.simulation;
 
+import java.util.concurrent.TimeUnit;
+
 import co.paralleluniverse.fibers.SuspendExecution;
 import desmoj.core.simulator.Model;
 import desmoj.core.simulator.SimProcess;
+import desmoj.core.simulator.TimeSpan;
 
 public class Task extends SimProcess {
+	String name;
+	private int executionTime;
+	private int deadline;
+	private int period;
+	private int offset;
+	private SimModel model;
+
+	public Task(String name, int executionTime, int deadline, int period, int offset, Model model) {
+		super(model, name, false);
+		this.name = name;
+		this.executionTime = executionTime;
+		this.deadline = deadline;
+		this.period = period;
+		this.offset = offset;
+		this.model = (SimModel) model;
+	}
 
 	public Task(Model arg0, String arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void lifeCycle() throws SuspendExecution {
-		// TODO Auto-generated method stub
-
+		while (true) {
+			Job job = new Job(this, name, executionTime, deadline, model);
+			job.activateAfter(this);
+			hold(new TimeSpan(period, TimeUnit.MILLISECONDS));
+		}
 	}
 
 }
